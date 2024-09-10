246cc756cdedcc66146f23cbd6d7a9685ad63cc6
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-10 15:49:43 -0400

    updates

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,20 +1,5 @@
-let experiment = "Stroop";
+let response1 = prompt('What is 5 + 3');
+console.log(response);
 
-console.log(typeof experiment); //string
-console.log(experiment.charAt(1)); //'t'
-
-let trialCountMax = 20;
-console.log(typeof trialCountMax) //number
-console.log(trialCountMax.charAT(0))
-
-// TODO: Randomize colors
-let colors = ('red', 'green', 'blue')
-
-alert('Welcome to the' + experiment + 'experiment');
-
-trialCountMax = 40;
-
-//At the halfway point we will display a pause screen
-let halfWayCount = trialCountMax / 2;
-
-//console.log(halfWayCount); //Expected 20
+let response2 = prompt('What is 5 + 5');
+console.log(response);
