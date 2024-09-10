09c1124d3ec89c038f13ded74ae95bd55f2f2499
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-10 15:23:07 -0400

    updates

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+let experiment = "Stroop";
+
+console.log(typeof experiment); //string
+console.log(experiment.charAt(1)); //'t'
+
+let trialCountMax = 20;
+console.log(typeof trialCountMax) //number
+console.log(trialCountMax.charAT(0))
+
+// TODO: Randomize colors
+let colors = ('red', 'green', 'blue')
+
+alert('Welcome to the' + experiment + 'experiment');
+
+trialCountMax = 40;
+
+//At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+//console.log(halfWayCount); //Expected 20
