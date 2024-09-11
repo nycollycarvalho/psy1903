3522f9f5c0cfc2e8fbc2044cd31708d4d8b36c3b
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-10 22:16:04 -0400

    updates to math-response-time.

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -1,21 +1,28 @@
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.")
 
+start1 = Date.now();
 response1 = prompt("What is 3+5?")
 console.log(response1)
 alert("You answered " + response1)
+end1 = Date.now();
+responseTime1 = (end1 - start1) / 1000; //number
+alert("You answered " + response1 + " in " + responseTime1 + " seconds.");
 
+start2 = Date.now();
 response2 = prompt("What is 5+2?")
 console.log(response1)
 alert("You answered " + response2)
+end2 = Date.now();
+responseTime2 = (end2 - start2) / 1000;
+alert("You answered " + response2 + " in " + responseTime2 + " seconds.");
 
+start3 = Date.now();
 response3 = prompt("What is 6+4?")
 console.log(response3)
 alert("You answered " + response3)
+end3 = Date.now();
+responseTime3 = (end3 - start3) / 1000;
+alert("Your answered " + response3 + " in " + responseTime3 + " seconds.");
 
 alert("Thank you for your participation!")
 
-Date.now(start)
-Date.now(end)
-console.log(end - start / 1000)
-
-alert("You answered in " time)
