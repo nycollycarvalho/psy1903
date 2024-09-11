e02cf9bf6d8521b1c7c28ae750d1546cb4cf9a9c
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-10 22:38:27 -0400

    added random number option.

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -22,7 +22,20 @@ console.log(response3)
 alert("You answered " + response3)
 end3 = Date.now();
 responseTime3 = (end3 - start3) / 1000;
-alert("Your answered " + response3 + " in " + responseTime3 + " seconds.");
+alert("You answered " + response3 + " in " + responseTime3 + " seconds.");
+
+num1_1 = Math.floor(Math.random() * 10) + 1;
+num1_2 = Math.floor(Math.random() * 10) + 1;
+
+start4 = Date.now();
+response4 = prompt("What is " + num1_1 + " + " + num1_2 + "?");
+console.log(response4)
+alert("You answered " + response4)
+end4 = Date.now();
+responseTime4 = (end4 - start4) / 1000;
+alert("You answered " + response4 + " in " + responseTime4 + " seconds.")
+
 
 alert("Thank you for your participation!")
+
 
