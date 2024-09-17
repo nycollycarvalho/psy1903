0e1ca497d53d69f5e3c35a80b8f7ae3d14031da4
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-17 16:00:57 -0400

    created taskset3 folder and week3 outside resources file. reviewed input/output in practice folder

---
+++ docs/projects/basics/taskset3/app.js
@@ -0,0 +1,33 @@
+let form = document.getElementsByTagName('form')[0];
+let results = document.getElementById('results');
+
+let num1Output = document.getElementById("num1");
+let num2Output = document.getElementById("num2");
+
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+
+num1Output.innerHTML = num1;
+num2Output.innerHTML = num2;
+
+//equation.innerHTML = ("What is" + num1 + num2 "?" )
+
+// Listen for the form to be submitted
+form.addEventListener('submit', function (event) {
+
+    // Prevent the default form submission b
+    event.preventDefault();
+
+    //Stop the timer
+
+    //Calculate response time
+
+    // Collect the response
+    let response = form.elements['response'].value;
+
+    // Report the results
+    results.innerHTML = 'Hello ' + response + '!';
+
+    // Hide the form
+    form.style.display = "none";
+});
---
+++ outside-resources/week3.md
@@ -0,0 +1,7 @@
+Outside Resources Log - Week 3
+
+AI Prompts
+
+
+Outside sites
+
