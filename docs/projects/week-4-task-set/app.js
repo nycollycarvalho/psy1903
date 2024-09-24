38c0edd3d3ae6b4acbb7ac3340d63fc026d90b15
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-24 15:44:56 -0400

    created week 4 folder; created and ran celsiusToFahrenheit function.

---
+++ docs/projects/week-4-task-set/app.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(C, F) {
+
+    let fahrenheit = (F = (C * 1.8) + 32)
+    return fahrenheit
+}
+
+console.log(celsiusToFahrenheit(10));
+console.log(celsiusToFahrenheit(-5));
