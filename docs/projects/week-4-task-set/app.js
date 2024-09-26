75fd5cfff3ba7c0375eaab5cb1b878bbe7ea969a
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-09-26 18:53:01 -0400

    convertTemp exercise.

--- docs/projects/week-4-task-set/app.js
+++ docs/projects/week-4-task-set/app.js
@@ -1,7 +1,21 @@
-function celsiusToFahrenheit(C, F) {
+/*function celsiusToFahrenheit(C, F) {
     let temp = (F = (C * 1.8) + 32);
     return temp;
 }
 
 console.log(celsiusToFahrenheit(10));
 console.log(celsiusToFahrenheit(-5));
+*/
+
+function convertTemp(temp, converTo) {
+
+    if (converTo == "c") {
+        return ((temp - 32) / 1.8)
+    } else if (converTo == "f") {
+        return ((temp * 1.8) + 32)
+    }
+
+}
+
+console.log(convertTemp(10, 'c'));
+console.log(convertTemp(10, 'f')); 
