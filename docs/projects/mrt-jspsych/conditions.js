49c5511f89193c7b11863e5637dfb683601efcc5
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-10-11 20:35:58 -0400

    question 3 task set 5.

---
+++ docs/projects/mrt-jspsych/conditions.js
@@ -0,0 +1,14 @@
+let conditions = [];
+
+
+for (let i = 0; i < 3; i++) {
+
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
+    let answer = num1 + num2;
+
+    conditions.push({ num1: num1, num2: num2, answer: answer });
+
+}
+
+console.log(conditions);
