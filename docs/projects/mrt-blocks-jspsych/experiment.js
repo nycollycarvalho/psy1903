ee23cfc8c527a5980847601a3dfa3b86f038c0ef
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-10-11 23:10:42 -0400

    question3 taskset6

--- docs/projects/mrt-blocks-jspsych/experiment.js
+++ docs/projects/mrt-blocks-jspsych/experiment.js
@@ -14,6 +14,22 @@ let welcomeTrial = {
 };
 timeline.push(welcomeTrial);
 
+let likert_scale = [
+    "Strongly Disagree",
+    "Disagree",
+    "Neutral",
+    "Agree",
+    "Strongly Agree"
+];
+let likertSurvey = {
+    type: jsPsychSurveyLikert,
+    questions: [
+        { prompt: "I enjoy solving math problems.", labels: likert_scale },
+        { prompt: "I find math easy.", labels: likert_scale },
+    ]
+};
+timeline.push(likertSurvey);
+
 
 for (let block of conditions) {
 
