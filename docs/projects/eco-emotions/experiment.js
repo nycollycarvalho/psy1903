0c67a4df22ef9727e80cf3fa26c3159b8e25a971
Nycolly Carvalho <nycollycarvalho.estudar@gmail.com>
2024-10-28 21:43:47 -0400

    updatee

--- docs/projects/eco-emotions/experiment.js
+++ docs/projects/eco-emotions/experiment.js
@@ -84,166 +84,180 @@ for (let block of conditions) {
     timeline.push(blockInstructions);
 
 
-    let blockConditions = jsPsych.randomization.repeat(block.trial, 1);
+    for (let block of conditions) {
 
-
-    for (let condition of blockConditions) {
-        let conditionTrial = {
+        // Instructions
+        let blockInstructions = {
             type: jsPsychHtmlKeyboardResponse,
             stimulus: `
-       <p class='category1'><b>${block.categories[0]}</b> (press 'F')</p>
-       <p class='category2'><b>${block.categories[1]}</b> (press 'J')</p>
-       <p class='stimulusWord'>${condition.word}</p>
-       `,
-            choices: ['f', 'j'],
-            data: {
-                trialType: 'iat',
-                word: condition.word,
-                expectedCategory: condition.expectedCategory,
-                expectedCategoryAsDisplayed: condition.expectedCategoryAsDisplayed,
-                leftCategory: block.categories[0],
-                rightCategory: block.categories[1],
-                collect: true,
-            },
-            on_finish: function (data) {
-                if (data.response === data.expectedResponse) {
-                    data.correct = true;
-                } else {
-                    data.correct = false;
+        <h2>Part ${conditions.indexOf(block) + 1} of 4</h2>
+        <p>In this part, the two categories will be: <b>${block.categories[0]}</b> and <b>${block.categories[1]}</b>.</p>
+        <p>If the word should be categorized into the <b>${block.categories[0]}</b> category, then press <span class='key'>F</span>.</p>
+        <p>If the word should be categorized into the <b>${block.categories[1]}</b> category, then press <span class='key'>J</span>.</p>
+        <p>Press <span class='key'>SPACE</span> to start.</p>
+    `,
+            choices: [' '],
+        };
+        timeline.push(blockInstructions);
+
+        // Trials
+        let blockConditions = jsPsych.randomization.repeat(block.trial, 1);
+
+        for (let condition of blockConditions) {
+            let conditionTrial = {
+                type: jsPsychHtmlKeyboardResponse,
+                stimulus: `
+            <p class='category1'><b>${block.categories[0]}</b> (press 'F')</p>
+            <p class='category2'><b>${block.categories[1]}</b> (press 'J')</p>
+            <p class='stimulusWord'>${condition.word}</p>
+            `,
+                choices: ['f', 'j'],
+                data: {
+                    trialType: 'iat',
+                    word: condition.word,
+                    expectedCategory: condition.expectedCategory,
+                    expectedCategoryAsDisplayed: condition.expectedCategoryAsDisplayed,
+                    leftCategory: block.categories[0],
+                    rightCategory: block.categories[1],
+                    collect: true,
+                },
+                on_finish: function (data) {
+                    if (data.response === data.expectedResponse) {
+                        data.correct = true;
+                    } else {
+                        data.correct = false;
+                    }
                 }
             }
+            timeline.push(conditionTrial);
+
+            let fixationTrial = {
+                type: jsPsychHtmlKeyboardResponse,
+                stimulus: `<p class='fixation'>+</p>`,
+                choices: ['NO KEYS'],
+                trial_duration: 250
+            };
+            timeline.push(fixationTrial);
         }
-        timeline.push(conditionTrial);
-
-
-        let fixationTrial = {
-            type: jsPsychHtmlKeyboardResponse,
-            stimulus: `<p class='fixation'>+</p>`,
-            choices: ['NO KEYS'],
-            trial_duration: 250
-        };
-        timeline.push(fixationTrial);
     }
 
-}
-
-let likert_scale = [
-    "Never",
-    "Rarely",
-    "Sometimes",
-    "Often",
-    "Almost always"
-];
+    let likert_scale = [
+        "Never",
+        "Rarely",
+        "Sometimes",
+        "Often",
+        "Almost always"
+    ];
 
 
-let likertSurvey = {
-    type: jsPsychSurveyLikert,
-    preamble: `
+    let likertSurvey = {
+        type: jsPsychSurveyLikert,
+        preamble: `
    <h1 class='task3name'>Task 3</h1>
    <p>Please answer the following 10 questions.</p>
    `,
-    button_label: 'sumbmit',
-    questions: [
-        { prompt: "Thinking about climate change makes it difficult for me to concentrate.", labels: likert_scale },
-        { prompt: "Thinking about climate change makes it difficult for me to sleep.", labels: likert_scale },
-        { prompt: "My concerns about climate change make it hard for me to have fun with my family or friends.", labels: likert_scale },
-        { prompt: "My concerns about climate change undermine my ability to work to my potential.", labels: likert_scale },
-        { prompt: "My friends say I think about climate change too much.", labels: likert_scale },
-        { prompt: "I have noticed a change in a place that is important to me due to climate change.", labels: likert_scale },
-        { prompt: "I wish I behaved more sustainably.", labels: likert_scale },
-        { prompt: "I recycle.", labels: likert_scale },
-        { prompt: "I try to reduce my behaviors that contribute to climate change.", labels: likert_scale },
-        { prompt: "I feel guilty if I waste energy.", labels: likert_scale },
+        button_label: 'sumbmit',
+        questions: [
+            { prompt: "Thinking about climate change makes it difficult for me to concentrate.", labels: likert_scale },
+            { prompt: "Thinking about climate change makes it difficult for me to sleep.", labels: likert_scale },
+            { prompt: "My concerns about climate change make it hard for me to have fun with my family or friends.", labels: likert_scale },
+            { prompt: "My concerns about climate change undermine my ability to work to my potential.", labels: likert_scale },
+            { prompt: "My friends say I think about climate change too much.", labels: likert_scale },
+            { prompt: "I have noticed a change in a place that is important to me due to climate change.", labels: likert_scale },
+            { prompt: "I wish I behaved more sustainably.", labels: likert_scale },
+            { prompt: "I recycle.", labels: likert_scale },
+            { prompt: "I try to reduce my behaviors that contribute to climate change.", labels: likert_scale },
+            { prompt: "I feel guilty if I waste energy.", labels: likert_scale },
 
 
-    ]
-};
-timeline.push(likertSurvey);
+        ]
+    };
+    timeline.push(likertSurvey);
 
 
 
 
-let resultsTrial = {
-    type: jsPsychHtmlKeyboardResponse,
-    choices: ['NO KEYS'],
-    async: false,
-    stimulus: `
+    let resultsTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        choices: ['NO KEYS'],
+        async: false,
+        stimulus: `
        <h1>Please wait...</h1>
        <p>We are saving the results of your inputs.</p>
        `,
-    on_start: function () {
+        on_start: function () {
 
 
-        let prefix = 'eco-emotions';
-        let dataPipeExperimentId = 'qtpiSXvcWypL';
-        let forceOSFSave = false;
+            let prefix = 'eco-emotions';
+            let dataPipeExperimentId = 'qtpiSXvcWypL';
+            let forceOSFSave = false;
 
 
-        // Filter and retrieve results as CSV data
-        let results = jsPsych.data
-            .get()
-            .filter({ collect: true })
-            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
-            .csv();
-
-
-
-
-        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+            // Filter and retrieve results as CSV data
+            let results = jsPsych.data
+                .get()
+                .filter({ collect: true })
+                .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+                .csv();
 
 
 
 
-        let isLocalHost = window.location.href.includes('localhost');
+            let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
 
 
-        let destination = '/save';
-        if (!isLocalHost || forceOSFSave) {
-            destination = 'https://pipe.jspsych.org/api/data/';
+
+
+            let isLocalHost = window.location.href.includes('localhost');
+
+
+            let destination = '/save';
+            if (!isLocalHost || forceOSFSave) {
+                destination = 'https://pipe.jspsych.org/api/data/';
+            }
+
+
+            fetch(destination, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: '*/*',
+                },
+                body: JSON.stringify({
+                    experimentID: dataPipeExperimentId,
+                    filename: prefix + '-' + participantId + '.csv',
+                    data: results,
+                }),
+            }).then(data => {
+                console.log(data);
+                jsPsych.finishTrial();
+            })
         }
+    }
+    timeline.push(resultsTrial);
 
 
-        fetch(destination, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: '*/*',
-            },
-            body: JSON.stringify({
-                experimentID: dataPipeExperimentId,
-                filename: prefix + '-' + participantId + '.csv',
-                data: results,
-            }),
-        }).then(data => {
+    console.log(resultsTrial)
+
+
+    let debriefTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `<h1>Thank you!</h1><p>You can now close this tab.</p>`,
+        choices: ['NO KEYS'],
+        on_start: function () {
+            let data = jsPsych.data
+                .get()
+                .filter({ collect: true })
+                .ignore(['collect', 'stimulus', 'trial_type', 'trial_index', 'plugin_version'])
+                .csv();
+
+
             console.log(data);
-            jsPsych.finishTrial();
-        })
-    }
-}
-timeline.push(resultsTrial);
 
 
-console.log(resultsTrial)
+        }
+    };
+    timeline.push(debriefTrial);
 
 
-let debriefTrial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `<h1>Thank you!</h1><p>You can now close this tab.</p>`,
-    choices: ['NO KEYS'],
-    on_start: function () {
-        let data = jsPsych.data
-            .get()
-            .filter({ collect: true })
-            .ignore(['collect', 'stimulus', 'trial_type', 'trial_index', 'plugin_version'])
-            .csv();
-
-
-        console.log(data);
-
-
-    }
-};
-timeline.push(debriefTrial);
-
-
-jsPsych.run(timeline);
+    jsPsych.run(timeline);
